2aae0cb041635bad2b057ae5e156942614d2db40
hongbo <zhaohb94@163.com>
2023-08-06 21:36:40 +0800

    gather all function we needed into one file

---
+++ common/functions.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import numpy as np
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def softmax(x):
+    x = x - np.max(x)  # 溢出对策
+    return np.exp(x) / np.sum(np.exp(x))
+
+
